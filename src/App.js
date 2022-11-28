cab08c98c6795b101375a48efffc50f4ee71d61e
shanthilakshmi <guvalashanthi@gmail.com>
2022-11-28 15:08:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React/n This is my first React Application</h1>
+          <h1 className="App-title">Welcome to React
+                 This is my first React Application</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
